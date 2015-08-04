7691f84dfd08da42f50d85ee985112290348a74c
carmenbourlonlong@gmail.com <clong@wdtinc.com>
2015-08-03 22:40:02 -0500

    fixing lesson 5

--- fifth-lesson.js
+++ fifth-lesson.js
@@ -1,10 +1,19 @@
 var fs = require('fs');
+var path = require('path');
 var filePath = process.argv[2];
+var ext = process.argv[3];
+var result = [];
+var i;
 
 fs.readdir(filePath, function(err, files) {
   if(err) {
     console.log(err);
   } else {
-    console.log(files);
+    for(i = 0; i < files.length; i++) {
+      if(path.extname(files[i]) == '.' + ext) {
+        result.push(files[i]);
+      }
+    }
+    console.log(result.join('\n'));
   }
 });
